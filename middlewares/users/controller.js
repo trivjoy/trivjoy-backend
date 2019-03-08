75ac407743937b336d845026cac09c60075fb60a
ajinbramantio <ajinbramantio@gmail.com>
2019-03-08 16:51:27 +0700

    create deleted

--- middlewares/users/controller.js
+++ middlewares/users/controller.js
@@ -2,9 +2,11 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const User = require('./model')
 const controller = {
-  getRoot: (req, res, next) => {
+  getRoot: async (req, res, next) => {
+    const foundUser = await User.find()
     res.status(200).send({
-      message: 'Users'
+      message: 'Users',
+      foundUser
     })
   },
   postRegister: async (req, res, next) => {
@@ -12,56 +14,52 @@ const controller = {
     const hashedPassword = await bcrypt.hash(req.body.password, salt)
 
     const newUser = {
-      name: req.body.name,
-      email: req.body.email,
-      phone: req.body.phone,
-      gender: req.body.gender,
-      city: req.body.city,
-      avatar: req.body.avatar,
-      age: req.body.age,
-      address: req.body.address,
-      about: req.body.about,
+      ...req.body,
       salt: salt,
       password: hashedPassword
     }
+
     const result = await User.create(newUser)
     res.status(200).send({
       message: 'Register',
-      user: result
+      user: {
+        name: result.name,
+        email: result.email
+      }
     })
   },
   postLogin: async (req, res, next) => {
     const user = {
-      email: req.body.email,
-      password: req.body.password
+      ...req.body
     }
     // console.log(user)
-    const foundUser = await User.findOne({ email: user.email })
-    console.log(foundUser)
+    const foundUser = await User.findOne({ email: user.email }, null, {
+      lean: true
+    })
+    // console.log(foundUser)
     const comparePassword = await bcrypt.compare(
       user.password,
       foundUser.password
     )
-    // console.log(comparePassword)
-    const payload = {
-      sub: foundUser._id,
-      name: foundUser.name,
-      email: foundUser.email,
-      phone: foundUser.phone,
-      gender: foundUser.male,
-      city: foundUser.city,
-      avatar: foundUser.avatar,
-      age: foundUser.age
+    if (comparePassword === false) {
+      res.status(401).send({
+        error: 'error'
+      })
+    } else {
+      const { password, salt, ...user } = foundUser
+      console.log(foundUser)
+
+      // console.log(payload)
+      const token = await jwt.sign({ id: user._id }, process.env.SECRET)
+      // console.log('token : ', token)
+      res.status(200).send({
+        message: 'Login',
+        // authenticated: comparePassword,
+        token: token
+      })
     }
-    // console.log(payload)
-    const token = await jwt.sign(payload, process.env.SECRET)
-    // console.log('token : ', token)
-    res.status(200).send({
-      message: 'Login',
-      authenticated: comparePassword,
-      token: token
-    })
   },
+
   getProfile: async (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1]
     // console.log(token)
@@ -69,18 +67,17 @@ const controller = {
     const decodedUser = await jwt.verify(token, process.env.SECRET)
     console.log(decodedUser)
 
-    if (decodedUser.sub) {
-      const foundUser = await User.findById(decodedUser.sub, {
+    if (decodedUser.id) {
+      const foundUser = await User.findById(decodedUser.id, {
         salt: 0,
         password: 0
       })
       console.log(foundUser)
+      res.status(200).send({
+        message: 'Get my profile',
+        User: foundUser
+      })
     }
-
-    res.status(200).send({
-      message: 'Get my profile',
-      decodedUser: decodedUser
-    })
   },
   getUserById: async (req, res, next) => {
     const foundUser = await User.findOne({ _id: req.params.id })
@@ -100,6 +97,19 @@ const controller = {
       text: 'success',
       foundUser: user
     })
+  },
+  deleterUserById: async (req, res, next) => {
+    const foundUser = await User.findOneAndRemove({ _id: req.params.id })
+    if (foundUser === null) {
+      res.status(401).send({
+        text: `deleted failed`
+      })
+    } else {
+      res.status(200).send({
+        text: `delete by ${req.params.id} sucess`,
+        foundUser
+      })
+    }
   }
 }
 
--- middlewares/users/index.js
+++ middlewares/users/index.js
@@ -8,5 +8,6 @@ router.post('/register', controller.postRegister)
 router.post('/login', controller.postLogin)
 router.get('/profile', controller.getProfile)
 router.get('/:id', controller.getUserById)
+router.delete('/:id', controller.deleterUserById)
 
 module.exports = router
