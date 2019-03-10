88abcbcb511c30dee5c8bbedb2d4d598bb43320d
Bara Limbong <baralimbong@gmail.com>
2019-03-10 18:08:35 +0700

    create delete trip by id

--- middlewares/trips/controller.js
+++ middlewares/trips/controller.js
@@ -60,6 +60,33 @@ const controller = {
       message: 'Get Trip',
       result: result
     })
+  },
+  deleteTripById: async (req, res, next) => {
+    const token = req.headers.authorization.split(' ')[1]
+    const decodedUser = await jwt.verify(token, process.env.SECRET)
+    //console.log(decodedUser.id)
+    const getTrip = await Trip.findOne({ id: req.params.id })
+    if (getTrip === null) {
+      res.status(401).send({
+        text: `delete trip failed by ${req.params.id} because ${
+          req.params.id
+        } not there`
+      })
+    } else {
+      if (decodedUser.id == getTrip.id_user) {
+        const foundTrip = await Trip.findOneAndRemove({
+          id: req.params.id
+        })
+        res.status(200).send({
+          text: `delete trip succsess by ${req.params.id}`,
+          foundTrip: foundTrip
+        })
+      } else {
+        res.status(401).send({
+          text: `delete trip failed by ${req.params.id}`
+        })
+      }
+    }
   }
 }
 
