70c722bdd32d81de5ae608bd5f2624229f0eb6db
M Haidar Hanif <git@mhaidarhanif.com>
2019-03-12 18:54:11 +0700

    Prepare dummy/seed data and the endpoint

---
+++ middlewares/trips/data.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    author: '', // get user's _id from token's sub
+    title: 'Jalan Jalan ke Kota',
+    destination: 'Kota, Provinsi, Indonesia',
+    dateFrom: '2019-03-26T18:23:37.000Z',
+    dateTo: '2019-03-30T18:23:37.000Z',
+    budget: 2000000,
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg',
+    description: 'Keliling kota 100 kali yuk biar sehat.',
+    peopleMin: 1,
+    peopleMax: 5,
+    users_requested: [],
+    users_joined: []
+  },
+  {
+    author: '', // get user's _id from token's sub
+    title: 'Jalan Jalan ke Kota 2',
+    destination: 'Kota 2, Provinsi, Indonesia',
+    dateFrom: '2019-03-26T18:23:37.000Z',
+    dateTo: '2019-03-30T18:23:37.000Z',
+    budget: 2000000,
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg',
+    description: 'Keliling kota 100 kali yuk biar sehat.',
+    peopleMin: 1,
+    peopleMax: 5,
+    users_requested: [],
+    users_joined: []
+  }
+]
