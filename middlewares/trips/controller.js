d39d650f8e45104f7402904b508feee11f5119ca
ajinbramantio <ajinbramantio@gmail.com>
2019-03-12 12:46:13 +0700

    create auth and modified trips method createTrip and deketeTripById

--- middlewares/trips/controller.js
+++ middlewares/trips/controller.js
@@ -1,7 +1,5 @@
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-
 const Trip = require('./model')
+const auth = require('../auth/controller')
 
 const controller = {
   //////////////////////////////////////////////////////////////////////////////
@@ -14,24 +12,23 @@ const controller = {
 
   //////////////////////////////////////////////////////////////////////////////
   createTrip: async (req, res, next) => {
-    try {
-      const token = req.headers.authorization.split(' ')[1]
-      const decoded = await jwt.verify(token, process.env.SECRET)
-
+    const token = req.headers.authorization.split(' ')[1]
+    const decoded = await auth.verifyToken(token, process.env.SECRET)
+    if (!decoded.sub) {
+      res.status(401).send({
+        message: 'Wrong created trip'
+        // result: result
+      })
+    } else {
       const newTrip = {
         ...req.body,
         author: decoded.sub
       }
 
       const result = await Trip.create(newTrip)
-
       res.status(200).send({
         message: 'Created trip',
         result: result
-      })
-    } catch (error) {
-      res.status(401).json({
-        message: 'You are not authorized'
       })
     }
   },
@@ -57,22 +54,27 @@ const controller = {
   //////////////////////////////////////////////////////////////////////////////
   deleteTripById: async (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1]
-    const decoded = await jwt.verify(token, process.env.SECRET)
+    const decoded = await auth.verifyToken(token, process.env.SECRET)
     const result = await Trip.findOne({ id: Number(req.params.id) })
+    console.log(result)
 
     if (!result) {
       res.status(401).send({
         message: 'Trip is not found'
       })
+    } else if (String(result.author) !== decoded.sub) {
+      res.status(401).send({
+        message: 'user not there trip'
+      })
     } else {
       if (decoded.sub === String(result.author._id)) {
-        const deletedTrip = await Trip.findOneAndRemove({
-          id: Number(req.params.id)
-        })
+        // const deletedTrip = await Trip.findOneAndRemove({
+        //   id: Number(req.params.id)
+        // })
 
         res.status(200).send({
-          text: `Deleted trip success`,
-          deletedTrip: deletedTrip
+          text: `Deleted trip success`
+          // deletedTrip: deletedTrip
         })
       } else {
         res.status(401).send({
