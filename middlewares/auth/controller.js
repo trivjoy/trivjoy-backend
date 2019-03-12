4ffb77f8e18faee2c633fdb032ccb2b300ddcb35
M Haidar Hanif <git@mhaidarhanif.com>
2019-03-12 17:41:13 +0700

    Delete duplicated createToken function

--- middlewares/auth/controller.js
+++ middlewares/auth/controller.js
@@ -5,16 +5,19 @@ module.exports = {
   encryptPassword: async plainPassword => {
     const salt = await bcrypt.genSalt(10)
     const password = await bcrypt.hash(plainPassword, salt)
+
     return {
       salt,
       password
     }
   },
+
   comparePassword: async (password, hash) => {
     const authenticated = await bcrypt.compare(password, hash)
 
     return authenticated
   },
+
   createToken: async foundUser => {
     const payload = {
       sub: foundUser._id,
@@ -25,20 +28,11 @@ module.exports = {
 
     return token
   },
-  createToken: async foundUser => {
-    const payload = {
-      sub: foundUser._id,
-      id: foundUser.id
-    }
 
-    const token = await jwt.sign(payload, process.env.SECRET)
-    return token
-  },
   verifyToken: async token => {
     // console.log(decoded)
     try {
       const decoded = await jwt.verify(token, process.env.SECRET)
-
       return decoded
     } catch (error) {
       return error
